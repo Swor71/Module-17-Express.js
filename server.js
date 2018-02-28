ff3897a9ea9da7ebb042c8044d0997d56b403cd0
Marcin Swornowski <swor71@gmail.com>
2018-02-28 14:56:56 +0100

    Add Ex-17.4

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
-var bodyParser = require('body-parser');
 var express = require('express');
-var fs = require('fs');
-var stringifyFile;
-
 var app = express();
+app.use(express.static('assets'));
 
-app.use(bodyParser.json());
+app.get('/', function(req, res) {
+    res.sendFile('/index.html');
+});
 
-app.get('/getNote', function(req, res) {
-    fs.readFile('./test.json', 'utf8', function(err, data) {
-        if(err) throw err;
-        stringifyFile = data;
-        res.send(data);
-    })
+app.get('/userform', function(req, res) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.end(JSON.stringify(response));
+});
+
+var server = app.listen(3000, 'localhost', function(){
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example of listening ' + host + ':' + port);
+    
 })
-
-app.post('/updateNote/:note', function(req, res) {
-    stringifyFile = req.params.note;
-
-    fs.writeFile('./test.json', stringifyFile, function(err) {
-        if (err) throw err;
-        console.log('file updated');
-    })
-})
-
-app.listen(3000);
