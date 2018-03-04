86758ec419713f7ad91ef14ef929692166a2eea3
Marcin Swornowski <swor71@gmail.com>
2018-03-04 17:21:51 +0100

    Add Exercise-17.6

--- server.js
+++ server.js
@@ -1,32 +1,26 @@
 var express = require('express');
 var app = express();
-app.use(express.static('assets'));
 
-app.use('/store', function(req, res, next) {
-    console.log('The Store\'s Middleman here.. carry on..');
-    next();
-});
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use('/static', express.static('assets'));
 
 app.get('/', function(req, res) {
-    res.sendFile('/index.html');
+    res.render('login');
 });
 
-app.get('/userform', function(req, res) {
-    const response = {
-        first_name: req.query.first_name,
-        last_name: req.query.last_name
-    };
-    res.end(JSON.stringify(response));
+app.get('/auth/google', function(req, res) {
+    res.render('main');
 });
 
-app.get('/store', function(req, res) {
-    res.send('Welcome to the shop!');
+app.use(function (req, res, next) {
+    res.status(404).send('Sorry Buddy, cannot get you that.');
 });
 
 var server = app.listen(3000, 'localhost', function(){
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Example of listening ' + host + ':' + port);
-    
+    console.log('Listening ' + host + ':' + port);
 })
